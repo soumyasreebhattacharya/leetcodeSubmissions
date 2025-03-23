417355ecf8e5e647f0641ccc8bc42668f4d24d76
Soumyasree Bhattacharya <59442176+soumyasreebhattacharya@users.noreply.github.com>
2025-03-23 17:35:28 +0530

    Time: 6 ms (95.63%) | Memory: 45 MB (57.71%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
@@ -1,15 +1,23 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
+
+        //take two pointers i and j side by side elements of array from start.
+        // when the diff is bigger than k, shift i by 1. Id diff is small shift j by 1.
+        //handle duplicate elements in the nested while loop , if j is duplicate, increment j until duplicate element is not there.
+
+        
         Arrays.sort(nums);  
         int i = 0;
         int j = 1;
         int n = nums.length;
         int count = 0;
         while(i<n && j< n){
-            if(i==j && j<n-1){
+            //when i and j both becomes equal , increment j by 1
+            if(i==j && j<n-1){  
                 j++;
             }
-            if(i==j && j == n-1){
+            //when i and j both equal and they reach the end of array, then break from loop
+            if(i==j && j == n-1){ 
                 break;
             }
             int diff = nums[j] - nums[i];
